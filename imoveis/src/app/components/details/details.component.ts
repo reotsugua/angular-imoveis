49d05c873d8a9fae044d574d65a3388ac2146ae8
Renan Santos <renan.santos@grupoeuro17.com.br>
2025-04-30 19:35:41 -0300

    feat: customizando pagina detlahes

--- imoveis/src/app/components/details/details.component.ts
+++ imoveis/src/app/components/details/details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from '../../model/housing-location';
+import { HousingService } from '../../service/housing.service';
 
 @Component({
   selector: 'app-details',
@@ -7,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService: HousingService = inject(HousingService);
+  housingLocationId: number = Number(this.route.snapshot.params['id']);
 
+  housingLocation: HousingLocation | undefined = this.housingService.getHousingLocationById(this.housingLocationId);
 }
--- imoveis/src/app/components/housing-location/housing-location.component.ts
+++ imoveis/src/app/components/housing-location/housing-location.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { HousingLocation } from '../../model/housing-location';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
