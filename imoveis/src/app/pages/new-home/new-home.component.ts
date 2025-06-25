c3dc8aa28bc5c9d697811e2228bc4dfb680ac437
reotsugua <renanaugusto.ras@gmail.com>
2025-06-25 17:02:44 -0300

    correcao

--- imoveis/src/app/pages/new-home/new-home.component.ts
+++ imoveis/src/app/pages/new-home/new-home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { RecomendacoesComponent } from "../../components/recomendacoes/recomendacoes.component";
-import { FooterComponent } from '../../components/footer/footer.component';
 import { SectionComponent } from '../../components/section/section.component';
 
 @Component({
   selector: 'app-new-home',
-  imports: [HeaderComponent, RecomendacoesComponent, FooterComponent, SectionComponent],
+  imports: [HeaderComponent, RecomendacoesComponent, SectionComponent],
   templateUrl: './new-home.component.html',
   styleUrl: './new-home.component.css'
 })
